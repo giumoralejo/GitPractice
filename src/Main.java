66077861cecd4d0f4cae779296fdd10313ac8be5
giumoralejo <giu.o.i.moralejo@gmail.com>
2023-08-02 20:07:23 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("this is a new code!");
+        System.out.println("this is a new code!");
 
     }
 }
